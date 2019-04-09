5e533a1fb468a6db2d480daa774d7d99fc2006d7
nakamura <nakamura@1-10.com>
2019-04-09 19:12:56 +0900

    add

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
 # vue-image-buttons
 
-> A Vue.js project
+🦉写真サムネイルリンクボタン🦉  
+とりあえず、2種類  
+
+🦉<a href="https://nananakamura.github.io/c/vue-image-buttons/index.html" target="_blank">Demo</a>
+
+
+## Usage
+
+### type 1の場合
+#### HTML
+```
+<template>
+  <button-image-type1
+    url="リンク先"
+    title="タイトル"
+    subdescription="ちょっとした一言"
+    imagePath="画像パス"
+  />
+</template>
+```
+
+*新規ウィンドウとかには対応してない
+*タイトルや、ちょっとした一言の文字量にも対応していないので、ある程度調整は必要
+
+#### scripts
+
+.vueファイルコンポーネントフォルダーにコピペして、  
+componentsに追加？
+
+```
+import ButtonImageType1 from './components/ButtonImageType1'
+
+export default {
+  components: {
+    'button-image-type1' : ButtonImageType1,
+  }
+}
+```
+
 
 ## Build Setup
 
@@ -16,6 +54,13 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
+
+# SCSSを有効化
+npm install sass-loader node-sass --save-dev
+
+<style lang="scss">
+/* write SASS! */
+</style>
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1>image thumbnail buttons</h1>
+    <section>
+      <h2>type 1</h2>
+      <ul>
+        <li>
+          <button-image-type1
+            url="/"
+            title="Frog"
+            subdescription="a tailless amphibian with a short squat body, moist smooth skin, and very long hind legs for leaping."
+            imagePath="./static/assets/image1.jpg"
+          />
+        </li>
+        <li>
+          <button-image-type1
+            url="/"
+            title="Scenery"
+            subdescription="spectacular views of mountain scenery"
+            imagePath="./static/assets/image2.jpg"
+          />
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>type 2</h2>
+      <ul>
+        <li>
+          <button-image-type2
+            url="/"
+            title="SUMMER"
+            subdescription="夏の終わりの植物の花"
+            imagePath="./static/assets/image3.jpg"
+          />
+        </li>
+        <li>
+          <button-image-type2
+            url="/"
+            title="WINTER"
+            subdescription="北半球では12月から2月まで、南半球では6月から8月にかけての年の最も寒い時期です。"
+            imagePath="./static/assets/image4.jpg"
+          />
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ButtonImageType1 from './components/ButtonImageType1'
+import ButtonImageType2 from './components/ButtonImageType2'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'button-image-type1' : ButtonImageType1,
+    'button-image-type2' : ButtonImageType2
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 60px 200px;
+  h1 {
+    margin-bottom: 80px;
+  }
+  section + section {
+    margin-top: 100px;
+  }
+  ul {
+    padding: 0;
+    display: flex;
+    li {
+      width: 50%;
+      &:nth-child(even) {
+        margin-left: 20px;
+      }
+      &:nth-child(odd) {
+        margin-right: 20px;
+      }
+    }
+  }
 }
 </style>
